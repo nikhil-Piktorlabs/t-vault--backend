14e9c0cc1fee77266fae9f10f15def959f1edde2
Nikhil Gupta <nikhil@piktorlabs.com>
2021-10-13 15:46:00 +0530

    delete & update
    added both delete and update option for safes and secrets using id

--- models/safesModel.js
+++ models/safesModel.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 
 const SafesSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  owner: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 1,
+  },
+  owner: { type: String, required: true, trim: true, minlength: 1 },
   type: { type: String, required: true, enum: ["personal", "other"] },
   description: { type: String, trim: true, minlength: 10 },
   secrets: [{ name: String, added: { type: Date, default: Date.now } }],
--- routes/safesRoute.js
+++ routes/safesRoute.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const Safes = require("../models/safesModel");
 
 const router = express.Router();
@@ -9,7 +10,7 @@ router.get("/", async (req, res) => {
 
     res.send(safes);
   } catch (e) {
-    console.log(e.message);
+    console.log(e);
   }
 });
 
@@ -25,7 +26,47 @@ router.post("/", async (req, res) => {
     const savedSafe = await safe.save();
     res.send(savedSafe);
   } catch (e) {
-    console.log(e.message);
+    console.log(e);
+  }
+});
+
+router.delete("/:safeId", async (req, res) => {
+  try {
+    const removedSafe = await Safes.findByIdAndRemove(req.params.safeId);
+
+    res.send(removedSafe);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.patch("/:safeId", async (req, res) => {
+  try {
+    const updatedSafe = await Safes.findByIdAndUpdate(
+      req.params.safeId,
+      {
+        $set: req.body,
+        updated: Date.now(),
+      },
+      { new: true }
+    );
+
+    res.send(updatedSafe);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.patch("/:safeId/secrets/:secretId", async (req, res) => {
+  try {
+    const secretDeleted = await Safes.findByIdAndUpdate(req.params.safeId, {
+      $pull: { secrets: { _id: mongoose.Types.ObjectId(req.params.secretId) } },
+    });
+    console.log("yo");
+
+    res.send(secretDeleted);
+  } catch (e) {
+    console.log(e);
   }
 });
 
