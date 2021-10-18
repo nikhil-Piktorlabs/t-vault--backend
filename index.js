c8c6dc5f8ac7fc1dfd9d3118446fc03b088bb7ea
Nikhil Gupta <nikhil@piktorlabs.com>
2021-10-18 11:17:00 +0530

    deployment changes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const router = require("./router");
 require("dotenv/config");
 
 const app = new express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 
